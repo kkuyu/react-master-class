5f796e58fcab054bdc2f6aa2763643392ac26dba
kkuyu <devils67@naver.com>
2022-04-29 11:14:00 +0900

    Our First Styled Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Box1 = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: teal;
+`;
+
+const Box2 = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: tomato;
+`;
+
+const Text = styled.span`
+  color: white;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <div style={{ display: "flex" }}>
+        <div style={{ width: 100, height: 100, backgroundColor: "teal" }}>
+          <span style={{ color: "white" }}>Hi</span>
+        </div>
+        <div style={{ width: 100, height: 100, backgroundColor: "tomato" }}>
+          <span style={{ color: "white" }}>Hi</span>
+        </div>
+      </div>
+      <Father>
+        <Box1>
+          <Text>Hi</Text>
+        </Box1>
+        <Box2>
+          <Text>Hi</Text>
+        </Box2>
+      </Father>
     </div>
   );
 }
