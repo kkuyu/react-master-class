8f282c55b9f0f9bb93bc57ae7a36b09c4d9c03ed
kkuyu <devils67@naver.com>
2022-05-01 14:44:31 +0900

    Price Chart part Three

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -58,7 +58,22 @@ function Chart() {
             xaxis: {
               axisBorder: { show: false },
               axisTicks: { show: false },
-              labels: { show: false },
+              labels: {
+                show: false,
+                datetimeFormatter: { month: "mm 'yy" },
+              },
+              type: "datetime",
+              categories: data?.map((price) => price.time_close),
+            },
+            fill: {
+              type: "gradient",
+              gradient: { gradientToColors: ["#0be881"], stops: [0, 100] },
+            },
+            colors: ["#0fbcf9"],
+            tooltip: {
+              y: {
+                formatter: (value) => `$${value.toFixed(2)}`,
+              },
             },
           }}
         />
